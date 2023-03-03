c0cef0efb3909328ef9f9315141728425b739f84
Tifannie Truman <nietru143@gmail.com>
2023-03-03 15:10:47 -0500

    added the inquirer prompt module for ea readme subheader

--- Develop/index.js
+++ Develop/index.js
@@ -4,7 +4,53 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer.prompt = [
+  {
+    type: "input",
+    message: "What is the name/title of your project?",
+    name: "titlePrompt",
+  },
+  {
+    type: "input",
+    message: "Description of your project:",
+    name: "descriptionPrompt",
+  },
+  {
+    type: "input",
+    message: "If necessary, provide a table of contents for your project:",
+    name: "contentsPrompt",
+  },
+  {
+    type: "input",
+    message: "How does a user install your project app?",
+    name: "installationPrompt",
+  },
+  {
+    type: "input",
+    message: "Project Usage:",
+    name: "usagePrompt",
+  },
+  {
+    type: "input",
+    message: "Repository Liscence (if any):",
+    name: "licensePrompt",
+  },
+  {
+    type: "input",
+    message: "Project contributors/credits:",
+    name: "creditsPrompt",
+  },
+  {
+    type: "input",
+    message: "Project tests:",
+    name: "testsPrompt",
+  },
+  {
+    type: "input",
+    message: "Project questions:",
+    name: "questionsPrompt",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}

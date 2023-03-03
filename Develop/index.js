9f335efaa92874563dd51e3b49b0c6f64b1d0293
Tifannie Truman <nietru143@gmail.com>
2023-03-03 15:32:38 -0500

    got the prompts to run with the questions function promise

--- Develop/index.js
+++ Develop/index.js
@@ -4,56 +4,66 @@ const fs = require("fs");
 const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-inquirer.prompt = [
-  {
-    type: "input",
-    message: "What is the name/title of your project?",
-    name: "titlePrompt",
-  },
-  {
-    type: "input",
-    message: "Description of your project:",
-    name: "descriptionPrompt",
-  },
-  {
-    type: "input",
-    message: "If necessary, provide a table of contents for your project:",
-    name: "contentsPrompt",
-  },
-  {
-    type: "input",
-    message: "How does a user install your project app?",
-    name: "installationPrompt",
-  },
-  {
-    type: "input",
-    message: "Project Usage:",
-    name: "usagePrompt",
-  },
-  {
-    type: "input",
-    message: "Repository Liscence (if any):",
-    name: "licensePrompt",
-  },
-  {
-    type: "input",
-    message: "Project contributors/credits:",
-    name: "creditsPrompt",
-  },
-  {
-    type: "input",
-    message: "Project tests:",
-    name: "testsPrompt",
-  },
-  {
-    type: "input",
-    message: "Project questions:",
-    name: "questionsPrompt",
-  },
-];
+const questions = () =>
+  inquirer.prompt([
+    {
+      type: "input",
+      message: "What is the name/title of your project?",
+      name: "titlePrompt",
+    },
+    {
+      type: "input",
+      message: "Description of your project:",
+      name: "descriptionPrompt",
+    },
+    {
+      type: "input",
+      message: "If necessary, provide a table of contents for your project:",
+      name: "contentsPrompt",
+    },
+    {
+      type: "input",
+      message: "How does a user install your project app? :",
+      name: "installationPrompt",
+    },
+    {
+      type: "input",
+      message: "Project Usage:",
+      name: "usagePrompt",
+    },
+    {
+      type: "input",
+      message: "Repository Liscence:",
+      name: "licensePrompt",
+    },
+    {
+      type: "input",
+      message: "Project contributors/credits:",
+      name: "creditsPrompt",
+    },
+    {
+      type: "input",
+      message: "Project tests:",
+      name: "testsPrompt",
+    },
+    {
+      type: "input",
+      message: "Project questions:",
+      name: "questionsPrompt",
+    },
+  ]);
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+questions().then((response) => {
+  console.log(response);
+  const readmeBuild = `
+            <#>${response.name}
+
+    `;
+  fs.writeFile("readme.md", readmeBuild, (err) =>
+    err ? console.error(err) : console.log("Success!")
+  );
+});
 
 // TODO: Create a function to initialize app
 function init() {}

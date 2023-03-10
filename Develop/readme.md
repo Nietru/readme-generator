fa908631d512ce3bae274845536104503a05bd89
Tifannie Truman <nietru143@gmail.com>
2023-03-09 21:59:02 -0500

    finished and updated code in generateMarkdown js file

--- Develop/readme.md
+++ Develop/readme.md
@@ -1,37 +1,24 @@
-# fgdsaf
+# Readme Generator
+  ## Table of Contents:
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Testing](#testing)
+  - [Questions](#questions)
 
-## Table of Contents:
-
-- [Description](#description)
-- [Installation](#installation)
-- [Usage](#usage)
-- [Contribution](#contribution)
-- [Testing](#testing)
-- [Questions](#questions)
-
-## Description:
-
-fdsafdsa
-
-## Installation:
-
-fdsafdsa
-
-## Usage:
-
-fdsafdsa
-
-## License:
-
-## Contribution:
-
-fdsfdsa
-
-## Testing:
-
-grfdsgfd
-
-## Questions:
-
-- grfdgdfs
-- gfdsgfd
+  ## Description:
+  A CLI Nodejs way of having a professional readme generated with user prompts using inquirer pkg.
+  ## Installation:
+  fdrhnsikoanf
+  ## Usage:
+  hjfdiskoa
+  ## License:
+  [![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)
+  ## Contribution:
+  
+  ## Testing:
+  fdjnskla
+  ## Questions:
+  - tifannie123
+  - tifannie@email.com
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -9,34 +9,30 @@
 // choices: ["Apache", "Creative Commons", "Eclipse", "MIT", "None of the above"],
 // RESOURCE FOR LICENSE BADGES: https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba
 function generateMarkdown(data) {
+  console.log(data);
   let badgeCreated = "";
   if (data.repoLicense == "Apache") {
     badgeCreated =
       "[![License](https://img.shields.io/badge/License-Apache_2.0-red.svg)](https://opensource.org/licenses/Apache-2.0)";
-  }
-  if (data.repoLicense == "Creative Commons") {
+  } else if (data.repoLicense == "Creative Commons") {
     badgeCreated =
       "[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)](http://creativecommons.org/publicdomain/zero/1.0/)";
-  }
-  if (data.repoLicense == "Eclipse") {
+  } else if (data.repoLicense == "Eclipse") {
     badgeCreated =
       "[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)";
-  }
-  if (data.repoLicense == "MIT") {
+  } else if (data.repoLicense == "MIT") {
     badgeCreated =
       "[![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT)";
-  } else {
-    badgeCreated = "";
   }
   //data."name: ' ' from the index.js "
   return `# ${data.titlePrompt}
   ## Table of Contents:
-  [Description](#Description:)
-  [Installation](#Installation:)
-  [Usage](#usagePrompt)
-  [Contribution](#contributePrompt)
-  [Testing](#testPrompt)
-  [Questions](#github)
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Testing](#testing)
+  - [Questions](#questions)
 
   ## Description:
   ${data.descriptionPrompt}
